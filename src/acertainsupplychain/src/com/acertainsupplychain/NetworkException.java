78b8f4904effdeba0a6a4de81d8ebead1eeb2421
Arni <arnidat@gmail.com>
2014-01-16 18:05:41 +0100

    >> NetworkException << Added a new exception to throw network exceptions.

---
+++ src/acertainsupplychain/src/com/acertainsupplychain/NetworkException.java
@@ -0,0 +1,37 @@
+package com.acertainsupplychain;
+
+/**
+ * This exception flags that a network error occurred.
+ */
+@SuppressWarnings("serial")
+public class NetworkException extends OrderProcessingException {
+
+	/**
+	 * Constructor based on Exception constructors.
+	 */
+	public NetworkException() {
+		super();
+	}
+
+	/**
+	 * Constructor based on Exception constructors.
+	 */
+	public NetworkException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructor based on Exception constructors.
+	 */
+	public NetworkException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructor based on Exception constructors.
+	 */
+	public NetworkException(Throwable ex) {
+		super(ex);
+	}
+
+}

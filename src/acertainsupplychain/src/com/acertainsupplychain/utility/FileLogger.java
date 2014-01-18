988663c59962802409a8457b2b14e0fd9e60c8f3
Arni <arnidat@gmail.com>
2014-01-18 13:15:32 +0100

    >> FileLogger << Created the FileLogger.java class for writing logs files for durability, and added the filepath.properties file which determines the folder of where the log files should be written too.

---
+++ src/acertainsupplychain/src/com/acertainsupplychain/utility/FileLogger.java
@@ -0,0 +1,135 @@
+package com.acertainsupplychain.utility;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FileLogger {
+
+	public static void main(String[] args) {
+		FileLogger logger = null;
+		logger = new FileLogger("test", "arni\n");
+
+		logger.logToFile("asds1");
+	}
+
+	private final String fullPath;
+	private final static String FILE_TYPE = "txt";
+
+	private final static String PROPERTY_PATH = "filepath.properties";
+	private final static String PROPERTY_NAME = "path";
+	private final static String DEFAULT_FILENAME = "default_log";
+	private String initialContent;
+
+	// TODO Make custom exception instead of using root exception (Exception)
+	public FileLogger(String fileTitle, String initialContent) {
+		String title = fileTitle;
+		if (!validateFileTitle(fileTitle)) {
+			System.out.println("FileLogger: The given file title [" + fileTitle
+					+ "] is invalid, using default file title ["
+					+ DEFAULT_FILENAME + "]");
+			title = DEFAULT_FILENAME;
+		}
+		this.initialContent = initialContent;
+		if (this.initialContent == null) {
+			this.initialContent = "";
+		}
+
+		fullPath = createFullPath(title, FILE_TYPE);
+		System.out.println(fullPath);
+		// Create the file if it does not exist
+		logToFile("", false);
+	}
+
+	private boolean validateFileTitle(String title) {
+		if (title == null)
+			// throw new
+			// Exception("FileLogger: The given title cannot be null.");
+			return false;
+		if (title.isEmpty())
+			// throw new
+			// Exception("FileLogger: The given title cannot be empty.");
+			return false;
+		if (title.endsWith("/")) // TODO must be system generic
+			// throw new Exception(
+			// "FileLogger: The given title cannot be a folder.");
+			return false;
+		return true;
+	}
+
+	private String createFullPath(String fileTitle, String type) {
+
+		Properties properties = new Properties();
+		InputStream input = null;
+		String fullTitle = fileTitle + "." + type;
+
+		try {
+			input = getClass().getClassLoader().getResourceAsStream(
+					PROPERTY_PATH);
+			properties.load(input);
+			return appendTitleToFolder(properties.getProperty(PROPERTY_NAME),
+					fullTitle);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// As a last resort try to fall back to the user home directory
+		// http://stackoverflow.com/questions/585534/what-is-the-best-way-to-find-the-users-home-directory-in-java
+		System.out.println("FileLogger: Could not read " + PROPERTY_PATH
+				+ ", trying to use user 'home' directory instead.");
+		return appendTitleToFolder(System.getProperty("user.home"), fullTitle);
+	}
+
+	private String appendTitleToFolder(String folder, String title) {
+		if (!folder.endsWith("/")) {// TODO must be system generic
+			folder = folder + "/";// TODO must be system generic
+		}
+		return folder + title;
+	}
+
+	// TODO seems like a silly function
+	public void logToFile(String log, boolean addLineBreak) {
+		String string = log;
+		if (addLineBreak) {
+			string = log + "\n";
+		}
+		logToFile(string);
+	}
+
+	public void logToFile(String log) {
+		String content = log;
+
+		File file = new File(fullPath);
+
+		if (!file.exists()) {
+			content = initialContent + log;
+		}
+
+		if (file.getParentFile() != null) {
+			file.getParentFile().mkdirs();
+		}
+		FileWriter fileWriter = null;
+
+		try {
+			fileWriter = new FileWriter(file, true);
+			fileWriter.write(content);
+		} catch (Exception e) {
+			System.out.println("FileLogger: Could not write log to logfile.");
+			e.printStackTrace();
+		}
+		try {
+			fileWriter.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
